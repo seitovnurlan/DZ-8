4913f5185fabec2309e53e40e5987ce0c3cf2206
Nurlan Seitov <seitov@gmail.com>
2023-02-06 19:00:42 +0600

    Initial Commit

---
+++ DZ#8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ#8
+//
+//  Created by Nurlan Seitov on 6/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
